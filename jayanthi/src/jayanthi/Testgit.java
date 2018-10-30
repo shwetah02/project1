45b8ce4c22dedb1855ea7900eeff9d8ed1610799
Asma <Asma@Asma-PC>
2018-10-30 15:24:30 +0530

    aaa

---
+++ jayanthi/src/jayanthi/Testgit.java
@@ -0,0 +1,18 @@
+package jayanthi;
+
+import java.util.Stack;
+
+public class Testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Stack<String> s=new Stack();
+s.push("a");
+s.push("ac");
+s.push("af");
+s.push("ad");
+s.push("sa");
+System.out.println(s);
+	}
+
+}
