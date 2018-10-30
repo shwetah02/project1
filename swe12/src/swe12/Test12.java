fb0fddd2a24bcb0cd3be5e95909009fca8271e5d
Asma <Asma@Asma-PC>
2018-10-30 16:11:53 +0530

    gfde

---
+++ swe12/src/swe12/Test12.java
@@ -0,0 +1,20 @@
+package swe12;
+
+import java.util.Stack;
+
+public class Test12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<String> s=new Stack();
+		s.push("a");
+		s.push("ac");
+		s.push("af");
+		s.push("ad");
+		s.push("sav");
+		s.push("sa");
+		System.out.println(s);
+			}
+	
+
+}
