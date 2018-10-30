20865107d350757e7f066ad5cb0fd04a47aa312b
Asma <Asma@Asma-PC>
2018-10-30 16:20:42 +0530

    fgdgt

---
+++ sqw/src/sqw/Yesdd.java
@@ -0,0 +1,18 @@
+package sqw;
+
+import java.util.Stack;
+
+public class Yesdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<String> s=new Stack();
+		s.push("a");
+		s.push("ac");
+		s.push("af");
+		s.push("ad");
+		s.push("sav");
+		s.push("sa");
+		System.out.println(s);
+	}}
+
